24580d81eff2a8667d179dd2a14ff628fdd8dbf4
Evertrix <evgenigg2000@gmail.com>
2019-12-26 15:11:10 +0200

    Change of interface.

--- login.php
+++ login.php
@@ -46,42 +46,40 @@ mysqli_close($conn);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" type="image/x-icon" href="assets/images/twitter-icon-18-256.png">
-    <link rel="stylesheet" type="text/css" href="assets/CSS/style_login.scss">
+<!--    <link rel="stylesheet" type="text/css" href="assets/CSS/style_login.scss">-->
+    <link rel="stylesheet" type="text/css" href="assets/CSS/style_register.css">
     <link rel="stylesheet"
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
           crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
+<body id="body">
+
+<header>
+    <div>
+        <img src="assets/images/twitter-icon-18-256.png" class="tweet-image">
+    </div>
+</header>
 
 
-<div class='screen'>
+<div class="col-md">
+    <div id="logbox">
+        <form id="signup" method="post" action="login.php">
+            <h1>Log In To Your Account</h1>
+            <input name="username" type="text" placeholder="What's your username?" pattern="^[\w]{3,16}$"
+                   autofocus="autofocus" required="required" class="input pass"/>
 
-    <img class='logo' src='https://s.cdpn.io/15065/twitter-bird-dark-bgs.png'/>
+            <input name="password" type="password" placeholder="Choose a password" required="required"
+                   class="input pass"/>
 
-    <form method="post" action="login.php">
+            <input type="submit" name="login" value="Sign Up" class="inputButton">
 
-        <p class='large'>Welcome</p>
-
-        <div><input type="text" name="username" placeholder="Username"></div>
-        <br>
-        <div><input type="password" name="password" placeholder="Password"></div>
-
-        <div class='flo-r cf'>
-            <input class='flo-l' type='checkbox' id='remember-me'/>
-
-            <label class='small flo-l' for='remember-me'>Remember me</label>
-        </div>
-
-        <div class='signup-link flo-l clr-l'>
-            <p class='small'>You are new to twitter?</p>
-            <a class='medium' href="register.php" name="user-sign-up">Sign Up</a>
-        </div>
-        <input class='flo-r' type="submit" name="login" value="Sign In"/>
-    </form>
-
-</div>
+            <div class="text-center">
+                Don't have an account? <a href="register.php" name="user-sign-in" id="login_id">Sign Up</a>
+            </div>
+        </form>
+    </div>
 
 </body>
 </html>
